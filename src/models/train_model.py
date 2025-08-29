9920e2e40d061ea3df376331531372039b053649
Ubuntu <ubuntu@ip-172-31-83-134.ec2.internal>
2025-08-29 07:08:06 +0000

    initial commit

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -84,8 +84,9 @@ def main(args):
         mlflow.log_params(model_cfg['parameters'])
         mlflow.log_metrics({'mae': mae, 'r2': r2})
 
-        # Log and register model
+        print("------BEFORE------")
         mlflow.sklearn.log_model(model, "tuned_model")
+        print("------AFTER------")
         model_name = model_cfg['name']
         model_uri = f"runs:/{mlflow.active_run().info.run_id}/tuned_model"
 
